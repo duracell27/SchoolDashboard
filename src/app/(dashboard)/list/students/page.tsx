58a7c4eb9f93ab29fb5473c32906a1363c5b985d
Duracell27 <volodymyrshmidt@gmail.com>
2024-11-18 17:58:18 +0200

    add list pages for student teacher parent

---
+++ src/app/(dashboard)/list/students/page.tsx
@@ -0,0 +1,123 @@
+import Pagination from "@/components/Pagination";
+import Table from "@/components/Table";
+import TableSearch from "@/components/TableSearch";
+import { role, studentsData, teachersData } from "@/lib/data";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+type Student = {
+  id: number;
+  studentId: string;
+  name: string;
+  email?: string;
+  grade: number;
+  photo: string;
+  phone?: string;
+  class: string;
+  address: string;
+};
+
+const columns = [
+  {
+    header: "Info",
+    accessor: "info",
+  },
+  {
+    header: "Student ID",
+    accessor: "studentId",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Grade",
+    accessor: "grade",
+    className: "hidden md:table-cell",
+  },
+
+  {
+    header: "Phone",
+    accessor: "phone",
+    className: "hidden lg:table-cell",
+  },
+  {
+    header: "Address",
+    accessor: "address",
+    className: "hidden lg:table-cell",
+  },
+  {
+    header: "Actions",
+    accessor: "actions",
+  },
+];
+
+const StudentListPage = () => {
+  const renderRow = (item: Student) => (
+    <tr
+      key={item.id}
+      className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-purpleLight"
+    >
+      <td className="flex items-center gap-4 p-4">
+        <Image
+          src={item.photo}
+          alt="userphoto"
+          width={40}
+          height={40}
+          className="md:hidden xl:block size-10 rounded-full object-cover"
+        />
+        <div className="flex flex-col">
+          <h3 className="font-extrabold">{item.name}</h3>
+          <p className="text-xs text-gray-500">{item.class}</p>
+        </div>
+      </td>
+      <td className="hidden md:table-cell">{item.studentId}</td>
+      <td className="hidden md:table-cell">{item.grade}</td>
+      <td className="hidden md:table-cell">{item.phone}</td>
+      <td className="hidden md:table-cell">{item.address}</td>
+      <td>
+        <div className="flex items-center gap-2">
+          <Link href={`/list/teachers/${item.id}`}>
+            <button className="size-7 flex items-center justify-center rounded-full bg-sky">
+              <Image src={"/view.png"} alt="" width={16} height={16} />
+            </button>
+          </Link>
+          {role === "admin" && (
+            <button className="size-7 flex items-center justify-center rounded-full bg-purple">
+              <Image src={"/delete.png"} alt="" width={16} height={16} />
+            </button>
+          )}
+        </div>
+      </td>
+    </tr>
+  );
+
+  return (
+    <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
+      {/* top */}
+      <div className="flex items-center justify-between">
+        <h1 className="hidden md:block text-lg font-semibold">All Students</h1>
+        <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
+          <TableSearch />
+          <div className="flex items-center gap-4 self-end">
+            <button className="size-8 flex items-center justify-center rounded-full bg-yellow">
+              <Image src={"/filter.png"} alt="search" width={14} height={14} />
+            </button>
+            <button className="size-8 flex items-center justify-center rounded-full bg-yellow">
+              <Image src={"/sort.png"} alt="search" width={14} height={14} />
+            </button>
+            {role === "admin" && (
+              <button className="size-8 flex items-center justify-center rounded-full bg-yellow">
+                <Image src={"/plus.png"} alt="search" width={14} height={14} />
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
+      {/* list */}
+      <Table columns={columns} renderRow={renderRow} data={studentsData} />
+      {/* pagination */}
+      <Pagination />
+    </div>
+  );
+};
+
+export default StudentListPage;
--- src/app/(dashboard)/teacher/page.tsx
+++ src/app/(dashboard)/teacher/page.tsx
@@ -1,9 +1,25 @@
+import Announcements from '@/components/Announcements'
+import BigCalendar from '@/components/BigCalendar'
+import EventCalendar from '@/components/EventCalendar'
 import React from 'react'
 
 const TeacherPage = () => {
   return (
-    <div>TeacherPage</div>
+    <div className='p-4 flex-1 flex gap-4 flex-col xl:flex-row'>
+      {/* left */}
+      <div className="w-full xl:w-2/3">
+      <div className="h-full bg-white p-4 rounded-md">
+        <h1 className="text-xl font-semibold">Schedule</h1>
+        <BigCalendar/>
+      </div>
+      </div>
+      {/* right */}
+      <div className="w-full lg:w-1/3 flex flex-col gap-8">
+        
+        <Announcements/>
+      </div>
+    </div>
   )
 }
-
+ 
 export default TeacherPage
