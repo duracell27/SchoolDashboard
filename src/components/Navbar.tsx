49f6e88cc6b9c21a4c5317baaeb4817dda611378
Duracell27 <volodymyrshmidt@gmail.com>
2024-12-03 23:44:59 +0200

    add protected routes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,4 @@
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import React from "react";
 
@@ -27,13 +28,14 @@ const Navbar = () => {
           </span>
           <span className="text-[10px] text-gray-500 text-right">Admin</span>
         </div>
-        <Image
+        {/* <Image
           src={"/avatar.png"}
           alt="user"
           width={36}
           height={36}
           className="rounded-full"
-        />
+        /> */}
+        <UserButton/>
       </div>
     </div>
   );
